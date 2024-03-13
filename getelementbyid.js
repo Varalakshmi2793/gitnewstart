6914fdfc06a40168c17b67960cc3ec4477396210
Varalakshmi2793 <varaluckky.2@gmail.com>
2024-03-13 15:25:17 +0530

    feature signup working

---
+++ getelementbyid.js
@@ -0,0 +1,8 @@
+document.getElementById("main-heading").innerHTML ="Fruit World";
+document.getElementById("main-heading").style.color ="orange";
+document.getElementById("header").style.background="green";
+document.getElementById("header").style.borderBottomColor = "orange";
+document.getElementById("basket-heading").style.color="green"
+const test= document.getElementById("thanks")
+  
+test.innerHTML ="<p>Please visit us again</p>";
---
+++ method.js
@@ -0,0 +1,7 @@
+const k= document.getElementsByClassName("fruit");
+
+for(let i=0; i<k.length; i++)
+  {
+    k[i].style.fontWeight="bold";
+  }
+k[2].style.backgroundColor='yellow';
